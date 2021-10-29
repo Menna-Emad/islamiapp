031d25d14ef613c674105a7b82535f0f7cffc3dd
menna emad <mennaemadezzatashour285@gmail.com>
2021-10-29 17:44:16 +0200

    initial project added empty homepage screen

---
+++ lib/islamiapp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget{
+  static const String routeName='Homepage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+
+      ),
+    );
+  }
+}
